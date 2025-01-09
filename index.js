82a91797bc22bc5caee26d54125a62281c079284
Jared Alford <alford.jared@gmail.com>
2025-01-09 15:23:18 -0800

    cannot get loading overlay to appear

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 function contact(event) {
   event.preventDefault();
-  emailjs
+ /* emailjs
     .sendForm(
       'service_nq8xe6e',
       'template_s927r2i',
@@ -12,5 +12,11 @@ function contact(event) {
       'c2gg1wrHYnpmP0ntW'
     ).then(() => {
       console.log("this worked1");
-    })
+    }) */
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success'
+    loading.classList += " modal__overlay--visible"
+      setTimeout(() => {
+    console.log("it worked 1")
+   }, 500);
 }
