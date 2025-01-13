6c1ced145acc2375d09bdf9687d2af0c6f3a105c
Jared Alford <alford.jared@gmail.com>
2025-01-12 21:51:51 -0800

    can't get shapes to move

--- index.js
+++ index.js
@@ -2,21 +2,56 @@
 // service_nq8xe6e
 // c2gg1wrHYnpmP0ntW
 
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    shapes[i].style.transform = `translate(${x}px, ${y}py)`
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
 function contact(event) {
   event.preventDefault();
- /* emailjs
+  const loading = document.querySelector('.modal__overlay--loading');
+  const success = document.querySelector('.modal__overlay--success');
+  loading.classList += " modal__overlay--visible";
+  emailjs
     .sendForm(
       'service_nq8xe6e',
       'template_s927r2i',
       event.target,
       'c2gg1wrHYnpmP0ntW'
     ).then(() => {
-      console.log("this worked1");
-    }) */
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success'
-    loading.classList += " modal__overlay--visible"
-      setTimeout(() => {
-    console.log("it worked 1")
-   }, 500);
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    }).catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at alford.jared@gmail.com."
+      );
+    })
+  }
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
